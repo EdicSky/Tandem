57471ff36ca2c737ca50c9e0c3ced673c638ac94
yuyuko <yuyuko@yuyukode-MacBook-Pro.local>
2020-03-16 14:46:02 +0800

    存入公告圖片

--- src/pages/bulletin/Bulletin.js
+++ src/pages/bulletin/Bulletin.js
@@ -3,6 +3,8 @@ import { NavLink } from 'react-router-dom'
 import React from 'react'
 import News from '../bulletin/News'
 import Sales from '../bulletin/Sales'
+import LatestNews from '../../components/bulletin/LatestNews'
+import NewsIntroduction from '../../components/bulletin/NewsIntroduction'
 
 import '../../css/newsIndex.scss'
 import {
@@ -14,6 +16,7 @@ import {
   AiOutlineCaretLeft,
   AiOutlineCaretRight,
 } from 'react-icons/ai'
+import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io'
 function Bulletin() {
   return (
     <Router>
@@ -56,7 +59,7 @@ function Bulletin() {
             <div className="row d-flex justify-content-between ">
               <div className="col-md-7 ">
                 <div
-                  className="input_icons position-relative "
+                  className=" position-relative mobile_input"
                   style={{ marginBottom: 80 + 'px' }}
                 >
                   <AiOutlineSearch className="icon_s position-absolute"></AiOutlineSearch>
@@ -66,44 +69,8 @@ function Bulletin() {
                     className="form-control news_search"
                   />
                 </div>
-                <div className="card news_introduction">
-                  <img
-                    src="news_images/hades.jpg"
-                    className="card-img-top"
-                    alt="..."
-                  />
-                  <div className="news card-body">
-                    <div className="news_index_title">
-                      <h2 className="card-title">萬智牌：競技場》——新手指南</h2>
-                    </div>
-                    <div className="news_index_content">
-                      <p className="card-text">
-                        《萬智牌：競技場》，劃定時代的集換式卡牌遊戲衍生出的廣受好評的新版本，現已在Epic
-                        Games Store
-                        上線。就在今天！《萬智牌：競技場》使傳奇的卡牌遊戲比以往都容易上手。'無需付費即可暢玩，提供遊戲玩法的指導，且自動執行所有規則，在遊戲中助您一臂之力。如果您一直想試試《萬智牌》，現在就是加入的最好時機。
-                        Epic Games Store將是您通向世界頂尖集換式卡牌遊戲的通道。
-                      </p>
-                    </div>
-                    <div className="news_index_detail d-flex justify-content-between ">
-                      <div className="news_index_detail_icon_group d-flex justify-content-between">
-                        <NavLink to="#" className="vendor_name">
-                          世嘉(SEGA)
-                        </NavLink>
-                        <div className="category_group d-flex">
-                          <AiOutlineFolderOpen className="icon" />
-                          <NavLink to="/bulletin/news">新聞</NavLink>
-                        </div>
-                        <div className="time_group d-flex">
-                          <AiOutlineClockCircle className="icon" />
-                          <p className=" d-flex">2019.11.1</p>
-                        </div>
-                      </div>
-                      <NavLink to="/bulletin/news" className="more_btn btn">
-                        詳細內容
-                      </NavLink>
-                    </div>
-                  </div>
-                </div>
+                <NewsIntroduction />
+                <NewsIntroduction />
               </div>
               <div className="col-md-5 ">
                 <div className="input_icons position-relative">
@@ -116,33 +83,24 @@ function Bulletin() {
                 </div>
                 <div className="latest_news">
                   <h2>最新消息</h2>
-                  <div className="latest_news_group d-flex justify-content-between align-items-start">
-                    <div className="latest_news_img">
-                      <img src="news_images/starter_guide.png" alt="" />
-                    </div>
-                    <div className="latest_news_title">
-                      <NavLink to="/bulletin/news">
-                        <h4>《萬智牌：競技場》——新手指南</h4>
-                      </NavLink>
-                      <div className="time_group d-flex">
-                        <AiOutlineClockCircle className="icon" />
-                        <p className=" d-flex">2019.11.1</p>
-                      </div>
-                    </div>
-                  </div>
+                  <LatestNews />
+                  <LatestNews />
+                  <LatestNews />
+                  <LatestNews />
+                  <LatestNews />
                 </div>
                 <div className="category_section">
                   <h2>分類</h2>
-                </div>
-                <div className="news">
-                  <NavLink to="/bulletin/news">
-                    <h4>新聞公告(5)</h4>
-                  </NavLink>
-                </div>
-                <div className="sales">
-                  <NavLink to="/bulletin/news">
-                    <h4>優惠公告(4)</h4>
-                  </NavLink>
+                  <div className="news">
+                    <NavLink to="/bulletin/news">
+                      <h4>新聞公告(5)</h4>
+                    </NavLink>
+                  </div>
+                  <div className="sales">
+                    <NavLink to="/bulletin/news">
+                      <h4>優惠公告(4)</h4>
+                    </NavLink>
+                  </div>
                 </div>
               </div>
             </div>
